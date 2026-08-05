f2ad90b5119718ce9f7d9f7363d980a28a431e6b
Santiago Díaz <santidiazfurque0211@gmail.com>
2026-08-05 16:57:30 -0500

    Sesion 2 - Variables e input

---
+++ sesion_02.py
@@ -0,0 +1,12 @@
+nombre = input("¿Cual es tu nombre?")
+edad = int(input("¿Cuantos años tienes?"))
+ciudad = input("¿Donde vives?")
+comida = input("¿ Cual es tu comida favorita?")
+
+print("Hola,", nombre)
+
+print("Actualmente tienes", edad, "años")
+
+print("Vives en", ciudad)
+
+print("Tu comida favorita es", comida)
