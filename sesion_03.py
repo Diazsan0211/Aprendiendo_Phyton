108dab8d248ed7a77ecfd92bd139ea9de2e8cb11
Santiago Díaz <santidiazfurque0211@gmail.com>
2026-08-06 16:04:52 -0500

    Sesión 3 - Operadores, bool e if

---
+++ sesion_03.py
@@ -0,0 +1,48 @@
+
+print(8 * 5)
+print(8 - 5)
+print(8 / 5)
+print(8 / 2)
+print(8 // 5)
+print(8 // 2)
+print(9 // 2)
+print(9 % 2)
+print(10 // 3)
+print(10 % 3)
+print(8 > 5)
+print(8 < 5)
+print(8 == 5)
+print(8 != 5)
+es_mayor = 8 > 5
+print(es_mayor)
+edad = 21
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+edad = input(" Cuantos años tienes?")
+if int(edad) >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+edad = int(input(" Cuantos años tienes?"))
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+edad = int(float(input(" Cuantos años tienes?")))
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+edad = float(input(" Cuantos años tienes?"))
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+nombre = input("¿Cómo te llamas? ")
+
+if nombre == "Santiago":
+    print("Bienvenido")
+else:
+    print("No te conozco")
